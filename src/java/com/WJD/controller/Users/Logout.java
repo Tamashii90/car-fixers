de81a93f98b319467a3484b232a0fd93859bd8a6
Tamashii90 <patoboy90@hotmail.com>
2022-01-03 17:36:46 +0200

    add title in header

--- src/java/com/WJD/controller/Users/Logout.java
+++ src/java/com/WJD/controller/Users/Logout.java
@@ -7,10 +7,8 @@
 package com.WJD.controller.Users;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,7 +19,6 @@ public class Logout extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
         request.getSession().invalidate();
         response.sendRedirect(request.getContextPath());
     } 
